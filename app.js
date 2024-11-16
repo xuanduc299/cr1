617127181650b654862eddfdeae2505b6b04666d
xuanduc299 <xuanduc511@gmail.com>
2024-11-16 19:51:12 +0700

    ud3

--- app.js
+++ app.js
@@ -4,63 +4,63 @@ var textBox = document.querySelector(".textBox");
 var button = document.querySelector(".button");
 var textBtn = document.querySelector(".textBtn");
 var heartBox = document.querySelector(".heartBox");
-var cursor = document.querySelector(".buttonCLick img"); 
+var cursor = document.querySelector(".buttonCLick img");
 var heartItem1 = document.querySelector(".heartItem.item1")
 var heartItem2 = document.querySelector(".heartItem.item2")
 var heartItem3 = document.querySelector(".heartItem.item3")
 var count = 0;
 var percentWidth = 0;
 var heartLeft = -3.2;
-var progressLoad = setInterval(progressInterva,100)
+var progressLoad = setInterval(progressInterva, 100)
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
     button.style.transform = "scale(0.8)";
-    setTimeout(()=>{
+    setTimeout(() => {
         button.style.transform = "scale(1)";
         window.location.href = "./love/love.html"
-    },200)
+    }, 200)
 })
 
-function progressInterva(){
-    if(count == 100 && percentWidth == 100){
+function progressInterva() {
+    if (count == 100 && percentWidth == 100) {
         clearInterval(progressLoad)
-        percent.textContent = "Ok rùi đó:)";
+        percent.textContent = "Okii rùi đó 🤭";
         percent.style.letterSpacing = "1px";
         textBox.style.transform = "scale(1.3)"
         heartItem3.style.animation = "1s heartScale forwards"
-        setTimeout(()=>{
+        setTimeout(() => {
             textBox.style.transform = "scale(0)"
-        },400)
-        setTimeout(()=>{
+        }, 400)
+        setTimeout(() => {
             textBox.style.opacity = "0"
-        },600)
-        setTimeout(()=>{
-            button.style.transform= "scale(1)";
-        },800);
-        setTimeout(()=>{
+        }, 600)
+        setTimeout(() => {
+            button.style.transform = "scale(1)";
+        }, 800);
+        setTimeout(() => {
             button.style.background = "rgb(244,118,121)"
             button.style.width = "130px";
             button.style.borderRadius = "20px"
-        },1500)
-        setTimeout(()=>{
+        }, 1500)
+        setTimeout(() => {
             button.style.height = "40px";
-        },2000)
-        setTimeout(()=>{
+        }, 2000)
+        setTimeout(() => {
             textBtn.textContent = "Click me!"
             textBtn.style.color = "#fff"
-        },2500)
-        setTimeout(()=>{
+        }, 2500)
+        setTimeout(() => {
             cursor.style.opacity = "1";
-        },3000)
+        }, 3000)
     }
-    else{
-        if(count == 10){
+    else {
+        if (count == 10) {
             heartItem1.style.animation = "1s heartScale forwards"
         }
-        if(count ==46){
-            percent.style.color= "#fff"
+        if (count == 46) {
+            percent.style.color = "#fff"
         }
-        if(count == 60){
+        if (count == 60) {
             heartItem2.style.animation = "1s heartScale forwards"
         }
         count += 1;
